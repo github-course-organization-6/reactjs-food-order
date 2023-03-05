e65b65b19e2a9b04f90b18a2c4ea46cdcd0fd1dd
Matthias Droth <matthias.droth@gmail.com>
2023-03-05 02:28:10 +0100

    edit it hideCartHandler (off course)

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
 
   return (
     <CartProvider>
-      {cartIsShown && <Cart />}
+      {cartIsShown && <Cart onClose={hideCartHandler}/>}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
